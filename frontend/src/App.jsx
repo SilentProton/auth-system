4f108ab93a34bd1d8c0fd57333ee39dd0ffc7567
SilentProton <silentproton.25@gmail.com>
2025-09-27 20:39:21 +0530

    chore: initial frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import FloatingShape from "./components/FloatingShape"
+import SignUpPage from "./pages/SignUpPage"
+import LogInPage from "./pages/LogInPage"
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-blue-500 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape color="bg-blue-100" size="w-64 h-64" top="-5%" left="10%" delay={0} />
+      <FloatingShape color="bg-blue-100" size="w-48 h-48" top="70%" left="80%" delay={5} />
+      <FloatingShape color="bg-blue-100" size="w-32 h-32" top="40%" left="-10%" delay={2} />
+
+      <Routes>
+        <Route path='/' element={"Home"}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/login' element={<LogInPage/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
