0da59b527c20ba8adbef9911da62607da8dcbe1e
SilentProton <silentproton.25@gmail.com>
2025-09-27 12:38:08 +0530

    feat: add forgot password endpoint

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup, verifyEmail } from "../controllers/auth.controller.js";
+import { login, logout, signup, verifyEmail, forgetPassword } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.post('/verify-email', verifyEmail)
+router.post('/forget-password', forgetPassword)
 
 export default router;
