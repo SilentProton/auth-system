7ceed7f14353543226eb5cc7c923953a1f7c4564
SilentProton <silentproton.25@gmail.com>
2025-09-27 09:21:26 +0530

    feat(auth): setup verify email endpoint and welcome email template using Mailtrap

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -2,7 +2,7 @@ import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
 import { mailtrapClient, sender } from "./mailtrap.config.js"
 
 export const sendVerificationEmail = async (email, verificationToken) => {
-    const recipient = [{email}]
+    const recipient = [{ email }]
 
     try {
         const response = await mailtrapClient.send({
@@ -14,9 +14,31 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         })
 
         console.log("Email sent successfully", response);
-        
+
     } catch (error) {
         console.error(`Error sending verification ${error}`);
         throw new Error(`Error sending verification email: ${error}`)
+    }
+}
+
+export const sentWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+        const response =  await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: process.env.WELLCOME_EMAIL_TEMPLATE_UUID,
+            template_variables: {
+                "company_info_name": "auth-system",
+                "name": name
+            }
+        })
+
+        console.log("Welcome email sent successfully", response);
+        
+    } catch (error) {
+        console.log(`Error sending welcome email`, error);
+        throw new Error(`Error sending welcome email: ${error}`);        
     }
 }
