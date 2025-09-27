22ff18c5a1d2f370e5d16913764412bab123230b
SilentProton <silentproton.25@gmail.com>
2025-09-27 05:56:37 +0530

    chore: fix typo in user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -25,7 +25,7 @@ const UserSchema = new mongoose.Schema({
     },
     resetPasswordToken: String,
     resetPasswordExpiresAt: Date,
-    varificationToken: String,
+    verificationToken: String,
     verificationTokenExpiresAt: Date,
 }, {timestamps: true})
 
